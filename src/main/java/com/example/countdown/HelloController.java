88034c5fb9a48e29016af6932b0a43da3b539044
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-12-07 21:29:09 +0100

    LejartAzIdo függvény elkészítése

--- src/main/java/com/example/countdown/HelloController.java
+++ src/main/java/com/example/countdown/HelloController.java
@@ -2,18 +2,26 @@ package com.example.countdown;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    public Label megadottIdopont;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Countdown application");
-    }
+    public Label hatralevoIdo;
+
 
     public void inditas(ActionEvent actionEvent) {
 
+
+    }
+
+
+    private void lejartAzIdo() {
+        Alert alertAblak = new Alert(Alert.AlertType.NONE, "Lejárt az idő!", ButtonType.OK);
+        alertAblak.setTitle("Amikor lejár az idő");
+        alertAblak.show();
     }
 }
