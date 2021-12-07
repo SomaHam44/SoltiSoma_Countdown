f9972d38e1c4411c65a5009dd99e689eaa3aeb79
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-12-07 21:15:36 +0100

    Kinézet szebb megvalósítása

--- src/main/java/com/example/countdown/HelloController.java
+++ src/main/java/com/example/countdown/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.countdown;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,9 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Countdown application");
+    }
+
+    public void inditas(ActionEvent actionEvent) {
+
     }
 }
