a4a21c721c85be50ee25b3960b01038862ccd476
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-12-07 22:09:42 +0100

    Program készítésének folyamata, Timer és timertask

--- src/main/java/com/example/countdown/HelloController.java
+++ src/main/java/com/example/countdown/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.countdown;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -12,19 +13,53 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.zip.DataFormatException;
 
 public class HelloController {
     @FXML
     public TextField megadottIdopont;
     @FXML
     public Label hatralevoIdo;
+    private Timer datumIdoTimer;
 
 
     public void inditas(ActionEvent actionEvent) {
         String bekertIdo = megadottIdopont.getText();
-        LocalDateTime idopont = LocalDateTime.parse(bekertIdo, DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
-        Period datum = Period.between(LocalDate.now(), idopont.toLocalDate());
-        Duration idopontokKozott = Duration.between(LocalDateTime.now(), idopont);
+        try {
+            LocalDateTime idopont = LocalDateTime.parse(bekertIdo, DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+            Period datum = Period.between(LocalDate.now(), idopont.toLocalDate());
+            Duration idopontokKozott = Duration.between(LocalDateTime.now(), idopont);
+            if (!idopontokKozott.isNegative() || !idopontokKozott.isNegative()) {
+                datumIdoTimer = new Timer();
+                TimerTask timerTaskunk = new TimerTask() {
+                    @Override
+                    public void run() {
+                        Period datum = Period.between(LocalDate.now(), idopont.toLocalDate());
+                        Duration idopontokKozott = Duration.between(LocalDateTime.now(), idopont);
+                        int evunk = datum.getYears();
+                        int honapunk = datum.getMonths();
+                        int napunk = datum.getDays();
+                        int orank = idopontokKozott.toHoursPart();
+                        int percunk = idopontokKozott.toMinutesPart();
+                        int masodpercunk = idopontokKozott.toSecondsPart();
+                        Platform.runLater(() -> hatralevoIdo.setText(evunk + "." + honapunk + "." +
+                                napunk + " " + orank + ":" + percunk + ":" + masodpercunk));
+                    }
+                };
+                datumIdoTimer.schedule(timerTaskunk,1 ,1 );
+
+            }
+            else {
+                hatralevoIdo.setText("A dátum vagy idő nem lehet negatív szám!");
+            }
+        }
+        catch (Exception e) {
+            hatralevoIdo.setText("Nem megfelelő időpont!");
+
+        }
+
 
 
 
