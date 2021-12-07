32f0ee00cb161fb7aa2eb2cdb364b765ae867314
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-12-07 21:50:12 +0100

    Duration és Period a programban

--- src/main/java/com/example/countdown/HelloController.java
+++ src/main/java/com/example/countdown/HelloController.java
@@ -8,7 +8,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class HelloController {
     @FXML
@@ -18,6 +21,10 @@ public class HelloController {
 
 
     public void inditas(ActionEvent actionEvent) {
+        String bekertIdo = megadottIdopont.getText();
+        LocalDateTime idopont = LocalDateTime.parse(bekertIdo, DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+        Period datum = Period.between(LocalDate.now(), idopont.toLocalDate());
+        Duration idopontokKozott = Duration.between(LocalDateTime.now(), idopont);
 
 
 
